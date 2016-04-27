0d0e52caba578e48fe4bb5d6c9badc98f751d31e
Taryn King <tking2005@gmail.com>
2016-04-27 17:04:05 -0400

    updated

--- TestingPackage/MyString_Student.py
+++ TestingPackage/MyString_Student.py
@@ -25,29 +25,87 @@ class MyString():
     #Returns a string that consists of the substring between start and end indexes (both included) in the current string.
     #Index 1 corresponds to the first character in the current string.'''
     def getSubstring( self,start, end):
+        originalWord = self.str
+        wordCount = len(originalWord)
+        subString = ""
+        startSubString = start-1
+        endSubString=end
+        if startSubString>=0 and endSubString<=wordCount:
+            try:
+                for count in range(startSubString,endSubString):
+                    subString=subString+originalWord[count]
+            except IndexError:
+                return("Index out of bounds")
+            return subString
+        else:
+            return ("Index out of bounds")
 
-        return 1
     #Breaks the string down, and returns it as a character string
     def getCharList(self):
+        originalWord = self.str
+        wordCount = len(originalWord)
+        CharacterList=[]
+        for counter in range(0, wordCount):
+            CharacterList.append(originalWord[counter])
 
-        return 1
+        return CharacterList
+
+    # Returns the index of the first occurrence of a character in the current string.
+    # Index 1 corresponds to the first character in the current string.
+    # return 0 if no match is found
+    def indexOf(self, c):
+        originalWord = self.str
+        wordCount = len(originalWord)
+        wordIndex = c
+
+        for count in range(0, wordCount):
+            if originalWord[count] == wordIndex:
+                return count
+                vowels = vowels + self.str[a]
+            return originalWord[wordIndex]
 
     #Returns the index of the first occurrence of a character in the current string.
     #Index 1 corresponds to the first character in the current string.
     # return 0 if no match is found
-    def indexOf(self,c):
+    def indexOfChar(self,c):
+        originalWord = self.str
+        wordCount = len(originalWord)
+        wordIndex = c-1
 
-        return 0
+        if wordCount<c:
+            return 0
+        else:
+            return originalWord[wordIndex]
+
+
 
 
     # Removes all occurrences of the specified character from the current string.
     def removeChar(self,c):
+        originalWord = self.str
+        wordCount = len(originalWord)
+        removeCharater = c
+        newRemoveCharWord = ""
+        counter = 0
+        while counter < wordCount:
+            if originalWord[counter]==removeCharater:
 
-        return 1
+                counter += 1
+            else:
+                newRemoveCharWord = newRemoveCharWord+originalWord[counter]
+                counter += 1
+
+        return newRemoveCharWord
 
 
     #Invert the current string.
     def invert(self):
+        originalWord=self.str
+        wordCount = len(originalWord)
+        inverseWord=""
+        countDown = 1
+        while countDown < wordCount+1:
+            inverseWord = inverseWord+(originalWord[wordCount-countDown])
+            countDown += 1
 
-
-        return 1
+        return inverseWord
--- TestingPackage/anothertest.py
+++ TestingPackage/anothertest.py
@@ -1,4 +1,9 @@
 #from TestingPackage.MyString_Student import MyString
 from MyString_Student import MyString
-s=MyString("Hello")
+s=MyString("MyBigFatGreekWedding")
 print(s.getVowels())
+print(s.invert())
+print(s.removeChar("e"))
+print(s.indexOfChar(6))
+print(s.getCharList())
+print(s.getSubstring(1,4))
